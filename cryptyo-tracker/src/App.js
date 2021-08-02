9a3524638ec2e5b74e4f82f8891a655f03bdf7f0
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2021-08-02 12:50:05 -0400

    Figured out the error, I was not calling the image first, the issue was the I was requesting all the data before the iamge renders causing the toFixed fucntion not to work.

--- cryptyo-tracker/src/App.js
+++ cryptyo-tracker/src/App.js
@@ -1,24 +1,26 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Coin from './Coin';
 import './App.css';
-
-
+import Coin from './Coin';
 
 function App() {
-  const [coins, setCoins] = useState([])
-  const [search,setSearch] = useState('')
-  useEffect(() => {
-    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
-    .then(res =>{
-      setCoins(res.sdata)
-      console.log(res.data);
-    }).catch(error => console.log(error))
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+      )
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error));
   }, []);
 
   const handleChange = e => {
-    setSearch(e.target.value)
+    setSearch(e.target.value);
   };
 
   const filteredCoins = coins.filter(coin =>
@@ -26,30 +28,29 @@ function App() {
   );
 
   return (
-    <div className="coin-app">
-      <div className="coin-search">
-        <h1 className="coin-text"> Search a curreny </h1>
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
         <form>
           <input
-          className='coin-input'
-          type="text" 
-          onChange={handleChange} 
-          placeholder="Search"
-         
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
           />
-          </form>
+        </form>
       </div>
       {filteredCoins.map(coin => {
         return (
-        <Coin
-          key={coin.id}
-          name = {coin.name}
-          price = {coin.image}
-          symbol ={coin.symbol}
-          marketcap={coin.total_volume}
-          volume ={coin.market_cap}
-          image={coin.image}
-          priceChange ={coin.price_change_percentage_24th}
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
           />
         );
       })}
